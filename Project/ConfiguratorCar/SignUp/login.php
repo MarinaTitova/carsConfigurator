00e3e5e8567a072570b53417304fc2dff26b42e7
MarinaTitova <stasgordynets2@gmail.com>
2016-11-18 03:57:55 +0300

    Some changes in style.
    Some changes in style.

--- Project/ConfiguratorCar/SignUp/login.php
+++ Project/ConfiguratorCar/SignUp/login.php
@@ -2,25 +2,27 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Login Page</title>
-    <link href="style/style.css" rel="stylesheet" type="text/css">
+    <title>Login</title>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 
-<table bgcolor="#f2f2f2" style="padding:50px" align="center" width="550px">
+<table style="padding:50px" align="center" width="550px">
     <form action="" method="post">
-        <tr><td align="center" colspan="2"> <h1>Login Page</h1> </td></tr>
+        <td align="center" colspan="1"><a href="signup.php"><button type="button">SignUP</button></a></td></tr>
         <tr>
-            <td> Email : </td><td><input type="email" name="email"></td>
+            <td align="left" colspan="2"> <h1>Login</h1> </td>
+        </tr>
+        <tr>
+            <td> Login : </td><td><input type="text" name="login"></td>
         </tr>
         <tr>
             <td> Password : </td><td><input type="password" name="password"></td>
         </tr>
 
         <tr>
-            <td align="center" colspan="2"><input type="submit" name="submit"></td></tr>
-        <td align="center" colspan="2"><a href="sign-up.php"><button type="button">SignUP</button></a></td></tr>
+            <td align="center" colspan="2"><input type="submit" name="submit" value="Apply"></td></tr>
 
     </form>
 </table>
@@ -34,20 +36,21 @@ if(isset($_POST["submit"]))
         //Including dbconfig file.
         include 'configDB.php';
 
-        $email = $_POST["email"];
+        $login = $_POST["login"];
 
         $password = $_POST["password"];
 
         $EncryptPassword = md5($password);
 
-        $finalResult = mysql_query("SELECT * FROM signup WHERE email='$email' and Password = '$EncryptPassword'");
+        $finalResult = mysql_query("SELECT * FROM users_login WHERE login='$login' and password = '$EncryptPassword'");
 
         $confirm = mysql_fetch_array($finalResult);
-
         if(is_array($confirm)) {
 
             session_start();
+            $_SESSION['user_name'] = $login;
             $_SESSION['sid']=session_id();
+
             header("location:dashboard.php");
 
         } else {
